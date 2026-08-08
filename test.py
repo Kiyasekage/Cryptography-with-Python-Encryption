269274a37774554b30753e122726a89efa8077a1
Jeremiah S. Visbal || JSV <156052449+Kiyasekage@users.noreply.github.com>
2026-08-08 20:49:00 +0800

    updating function to print lower or uppercase letters depending on users input

--- test.py
+++ test.py
@@ -1,10 +1,14 @@
 import string
-def al():
+def al(p_ans):
     v = string.ascii_uppercase
     contain = []
+    if p_ans=="lowercase":
+        v = string.ascii_lowercase
     for strings in v:
         contain.append(strings)
     return contain
 print("Welcome to encrypting and decrypting your words/sentences")
-alphabet = al()
-print(alphabet[0])
+name = input("What's your name? ")
+ans = input("Do you want your input to be in lowercase or uppercase? ").lower()
+alphabet = al(ans)
+print(alphabet)
