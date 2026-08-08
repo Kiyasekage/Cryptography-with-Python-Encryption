f68e9ef5cf14a59ecf37cd8209525b79672e5b9d
Jeremiah S. Visbal || JSV <156052449+Kiyasekage@users.noreply.github.com>
2026-08-08 23:55:13 +0800

    add for loop to store letter's position in user's input and encrypt the word

--- test.py
+++ test.py
@@ -4,8 +4,12 @@ def shifting(p_shift,p_mess):
     alphabet = string.ascii_uppercase
     v = alphabet[p_shift-1:]+alphabet[0:p_shift-1]
     store = []
+    final = []
     for strings in p_mess:
         store.append(alphabet.index(strings))
+    for digits in store:
+        final.append(v[digits])
+    return final
 print("Welcome to encrypting and decrypting your words/sentences")
 name = input("What's your name? ")
 ans = input("Do you want your input to be in lowercase or uppercase? ").lower()
